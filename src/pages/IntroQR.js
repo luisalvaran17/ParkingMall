3e9a8d4bfa6a40a037f5c9f9defd2a384fe5a407
Stiven <stiven.pinzon@correounivalle.edu.co>
2020-08-07 01:25:01 -0500

    plantilla

---
+++ src/pages/IntroQR.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import scanQR from './images/scan.png';
+import './styles/introQR.css'
+
+export class IntroQR extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>qr code</h2>
+                <div className="container">
+                    <img src={scanQR} class="img-fluid" alt="Responsive image" />
+
+                </div>
+                <div className="container">
+                    <div className="row" style={{ paddingBottom: "10px" }}>
+                        <div className="col-12" >
+                            <button className="btn btn-primary btn-sm">Scan</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <button className="btn btn-warning btn-sm">Next</button>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        )
+    }
+}
+
+export default IntroQR
