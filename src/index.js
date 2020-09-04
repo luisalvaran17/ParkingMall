7da764685ea736523a02137827eb4519ddd45ea5
lclucumi <luz.lucumi@correounivalle.edu.co>
2020-09-03 22:02:11 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import IntroWelcome from './pages/IntroWelcome';
 import IntroQR from './pages/IntroQR';
-//import PaginaPrincipal from './pages/PaginaPrincipal';
 import Splash from './pages/Splash';
+import MainPage from './pages/MainPage';
 
 ReactDOM.render(
     <Splash />,
