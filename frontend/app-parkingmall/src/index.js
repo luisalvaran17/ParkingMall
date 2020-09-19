a258c747e1bb34e1be1795c9d0e815ea799763c2
Stiven <stiven.pinzon@correounivalle.edu.co>
2020-09-19 11:18:51 -0500

    add Login page without validation

--- frontend/app-parkingmall/src/index.js
+++ frontend/app-parkingmall/src/index.js
@@ -5,12 +5,11 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import HeaderAndFooter from './pages/HeaderAndFooter';
 import ScanQR from './pages/ScanQR';
-import IntroQR from './pages/IntroQR';
+import Login from './pages/Login';
 //import PaginaPrincipal from './pages/PaginaPrincipal';
-import MainPage from './pages/MainPage';
 
 ReactDOM.render(
-  <HeaderAndFooter />,
+  <Login />,
   document.getElementById('root')
 );
 
@@ -21,7 +20,4 @@ ReactDOM.render(
   document.getElementById('root')
 );*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ frontend/app-parkingmall/src/pages/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import logo from './images/logoPM.jpg'
+import './styles/Login.css'
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
+
+                    <div className="fadeIn first">
+                        <img src={logo} id="icon" alt="User Icon" />
+                    </div>
+
+                    <form>
+
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                    </form>
+
+                    <div id="formFooter">
+                        <a className="underlineHover" href="#">Forgot Password?</a>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
