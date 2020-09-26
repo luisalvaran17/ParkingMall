2b1c48ff0495c6e205c2b313b6cac5646831693e
Luis Albaran <luisalvaaranleav@gmail.com>
2020-09-26 09:14:22 -0500

    fixes layout  scan QR

--- frontend/app-parkingmall/src/containers/ScanQR.js
+++ frontend/app-parkingmall/src/containers/ScanQR.js
@@ -9,60 +9,63 @@ class ScanQR extends Component {
         var React = require('react');
         var QRCode = require('qrcode.react');
         return (
-            <div className='container'>
-                <div className="d-flex flex-column flex-md-row align-items-center p-6 px-md-0 mb-1 bg-white">
-                    <div className='marginContainerScanQR' >
-                        <div className='col-sm' style={{padding:'0'}}>
-                            <img className='logoParkingMall' src={logo} alt=""/>
-                            <h5 className="tittleBrand my-0 mr-md-auto">ParkingMall</h5>
+            <div style={{backgroundColor:"white"}}>
+                <div className='container'>
+                    <div className="d-flex flex-column flex-md-row align-items-center p-6 px-md-0 mb-1 bg-white">
+                        <div className='marginContainerScanQR' >
+                            <div className='col-sm' style={{padding:'0'}}>
+                                <img className='logoParkingMall' src={logo} alt=""/>
+                                <h5 className="tittleBrand my-0 mr-md-auto">ParkingMall</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className='marginContainerScanQR'>
-                    <div className='row'>
-                        <div className='col-sm-8 col-xl-8' >
-                            <div className='row'>
-                                <div className='col-sm'>
-                                    <h6 className='h6TitleScan'>SCAN QR</h6>
-                                </div>
-                            </div>
-                            <div className='row'>
-                                <div className='col-sm'>
-                                    <p className='textDescripcionQR'>Activa la cámara y apunta tu teléfono hacia esta pantalla para escanear el código.</p>
-                                </div>
-                            </div>
-                            <hr></hr>
-                            <div className='contenedorEspacioAsignado'>
-                                <div className='row' >
+                    <div className='marginContainerScanQR'>
+                        <div className='row'>
+                            <div className='col-sm-8 col-xl-8' >
+                                <div className='row'>
                                     <div className='col-sm'>
-                                        <h3>Espacio asignado</h3>
+                                        <h6 className='h6TitleScan'>SCAN QR</h6>
                                     </div>
                                 </div>
                                 <div className='row'>
-                                    <div className='col-sm'></div>
                                     <div className='col-sm'>
-                                        <div><h4>Piso</h4></div>
-                                        <div><h1>5</h1></div>
+                                        <p className='textDescripcionQR'>Activa la cámara y apunta tu teléfono hacia esta pantalla para escanear el código.</p>
                                     </div>
-                                    <div className='col-sm'>
-                                        <div><h4>Zona</h4></div>
-                                        <div><h1>5</h1></div>
+                                </div>
+                                <hr></hr>
+                                <div className='contenedorEspacioAsignado'>
+                                    <div className='row' >
+                                        <div className='col-sm'>
+                                            <h3>Espacio asignado</h3>
+                                        </div>
                                     </div>
-                                    <div className='col-sm'></div>
+                                    <div className='row'>
+                                        <div className='col-sm'></div>
+                                        <div className='col-sm'>
+                                            <div><h4>Piso</h4></div>
+                                            <div><h1>5</h1></div>
+                                        </div>
+                                        <div className='col-sm'>
+                                            <div><h4>Zona</h4></div>
+                                            <div><h1>5</h1></div>
+                                        </div>
+                                        <div className='col-sm'></div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className='col-sm-4 col-xl'>
+                                <div className='row'>
+                                    <img className='imageScanMobileQR' src={imageScanMobileQR} alt='Scan Qr'/>
                                 </div>
                             </div>
                         </div>
-                        <QRCode value="http://facebook.com" />
-                        <div className='col-sm-4 col-xl'>
-                            <div className='row'>
-                                <img className='imageScanMobileQR' src={imageScanMobileQR} alt='Scan Qr'/>
-                            </div>
-                        </div>
                     </div>
-                </div>
 
-                <Footer />
+                    
+                </div>
+            <Footer />
             </div>
         );
     }
