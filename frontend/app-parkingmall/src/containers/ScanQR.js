f806d59e3bef4f3f38c4ac3458374644bec63b4a
Luis Albaran <luisalvaaranleav@gmail.com>
2020-10-01 21:57:24 -0500

    qr code  generator

--- frontend/app-parkingmall/src/containers/ScanQR.js
+++ frontend/app-parkingmall/src/containers/ScanQR.js
@@ -1,19 +1,41 @@
 import { Component } from 'react';
 import logo from '../assets/images/logoPM.jpg'
-import imageScanMobileQR from '../assets/images/scanmobile.png'
+import imageScanMobileQR from '../assets/images/scanMobile.png'
 import '../assets/styles/components/head_foo_scanQR.css';
 import Footer from '../components/Footer' 
 
 class ScanQR extends Component {
+    state = {
+        cadenaEspacioAsignar: ["1", "2", "3"]
+    };
+    
+    convertirCadena = () => {
+    let arregloEspacio = this.state.cadenaEspacioAsignar;
+    let mensaje = "";
+    for (let i = 0; i < 1; i++) {
+        mensaje =
+        "Piso " +
+        arregloEspacio[0] +
+        " Zona " +
+        arregloEspacio[1] +
+        " Puesto " +
+        arregloEspacio[2];
+    }
+    //console.log(mensaje);
+    console.log(mensaje);
+    return mensaje;
+    };
+
     render() {
         var React = require('react');
         var QRCode = require('qrcode.react');
+ 
         return (
             <div style={{backgroundColor:"white"}}>
                 <div className='container'>
                     <div className="d-flex flex-column flex-md-row align-items-center p-6 px-md-0 mb-1 bg-white">
                         <div className='marginContainerScanQR' >
-                            <div className='col-sm' style={{padding:'0'}}>
+                            <div className='col-sm' style={{padding:'0', textAlign:'center'}}>
                                 <img className='logoParkingMall' src={logo} alt=""/>
                                 <h5 className="tittleBrand my-0 mr-md-auto">ParkingMall</h5>
                             </div>
@@ -36,11 +58,14 @@ class ScanQR extends Component {
                                 <hr></hr>
                                 <div className='contenedorEspacioAsignado'>
                                     <div className='row' >
-                                        <div className='col-sm'>
-                                            <h3>Espacio asignado</h3>
+                                        <div className='col-sm-12'>
+                                            <h1>Espacio asignado</h1>
+                                        </div>
+                                        <div className='col-sm-12'>
+                                            <h4>Mira la pantalla de tu celular</h4>
                                         </div>
                                     </div>
-                                    <div className='row'>
+                                    {/*<div className='row'>
                                         <div className='col-sm'></div>
                                         <div className='col-sm'>
                                             <div><h4>Piso</h4></div>
@@ -51,13 +76,16 @@ class ScanQR extends Component {
                                             <div><h1>5</h1></div>
                                         </div>
                                         <div className='col-sm'></div>
-                                    </div>
+                                    </div>*/}
                                 </div>
                             </div>
                             
                             <div className='col-sm-4 col-xl'>
                                 <div className='row'>
-                                    <img className='imageScanMobileQR' src={imageScanMobileQR} alt='Scan Qr'/>
+                                    {/*<img className='imageScanMobileQR' src={imageScanMobileQR} alt='Scan Qr'/>*/}
+                                    <img className='imageScanMobileQR'  src={imageScanMobileQR} alt='Scan Qr'/>
+                                    <QRCode value={this.convertirCadena()} fgColor="#eea236"  
+                                    style={{position:'relative', marginLeft:'90px', marginTop:'-300px'}}/>
                                 </div>
                             </div>
                         </div>
