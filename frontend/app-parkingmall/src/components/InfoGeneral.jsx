f19c1f021e509061cacdeff0cc69e8a5efa3a8a8
lclucumi <luz.lucumi@correounivalle.edu.co>
2020-10-07 17:19:05 -0500

    Update InfoGeneral.jsx

--- frontend/app-parkingmall/src/components/InfoGeneral.jsx
+++ frontend/app-parkingmall/src/components/InfoGeneral.jsx
@@ -4,15 +4,17 @@ import Grid from '@material-ui/core/Grid';
 import '../assets/styles/components/InfoGeneral.css';
 
 class InfoGeneral extends React.Component {
-  render() {
+
+  render(){
     return (
       <Grid item xs>
+          <br/>
           <h3><center>Información general</center></h3>
         <Divider variant="middle" />
           <div id='informacion'>
-              <div><h2>20 </h2> <h6><br/><center>Lugares <br/> disponibles</center></h6></div>
-              <div><h2>40 </h2> <h6><br/><center>Lugares <br/> reservados</center></h6></div>
-              <div><h2>20 </h2> <h6><br/><center>Total <br/> vehículos</center></h6></div>
+              <div><h2>{this.props.car.carAvailable} </h2> <h6><br/><center>Lugares <br/> disponibles</center></h6></div>
+              <div><h2>{this.props.car.carReserved} </h2> <h6><br/><center>Lugares <br/> reservados</center></h6></div>
+              <div><h2>{this.props.car.carNumber} </h2> <h6><br/><center>Total <br/> vehículos</center></h6></div>
           </div>
       </Grid>
     );
